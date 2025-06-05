ca660da7e2de2cf6c156163977669a4cf2c98910
Bhagabanta-Giri <bhagabantagiri@gmail.com>
2025-06-05 21:19:03 +0530

    Edited Pairfinder - 1
    Added ratio feature to pairfinder

--- Pairfinder.py
+++ Pairfinder.py
@@ -1,21 +1,27 @@
 # 💘 Pair Finder: Like Tinder, but for lonely nucleotides.
 
-org_dna_seq = input("Please input the DNA sequence: ")
+org_dna_seq = input("Please input the DNA sequence: ").upper()
+print (org_dna_seq)
 
 # Our matchmaker's toolkit
 comp_dna_seq = []
 def_protein = 0
+ndef_protein = 0
 
 # Swipe right on every base in the input string
 for protein in org_dna_seq:
-    if protein == "A" or protein == "a":
+    if protein == "A":
         comp_dna_seq.append("T")
-    elif protein == "T" or protein == "t":
+        ndef_protein += 1
+    elif protein == "T":
         comp_dna_seq.append("A")
-    elif protein == "G" or protein == "g":
+        ndef_protein += 1
+    elif protein == "G":
         comp_dna_seq.append("C")
-    elif protein == "C" or protein == "c":
+        ndef_protein += 1
+    elif protein == "C":
         comp_dna_seq.append("G")
+        ndef_protein += 1
     else:
         comp_dna_seq.append("*")
         def_protein += 1
@@ -28,4 +34,11 @@ if def_protein == 0:
     print("It's a perfectly healthy DNA! No drama here. 💖")
 else:
     print("The DNA is defective at", str(def_protein), "points.")
+
+
+if ndef_protein == 0:
+    print("This sequence is straight-up nonsense. No valid bases found. 🤡")
+elif (def_protein / ndef_protein) >= 1:
     print("Immediate DNA cleavage is recommended 🔪🧬")
+else:
+    print("DNA is in safe stage.\nRectification is recommended.")
